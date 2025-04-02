2da945ee28ad75f73250c648b211bdcc11d422dc
ashwani-bhadani <ashwani.bhadani.3289@gmail.com>
2025-04-03 01:58:54 +0530

    CHORE: leanrning in progress

--- 05mytime/main.go
+++ 05mytime/main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
-	"time" // Go's built-in time package
+	"time"
 )
 
 func main() {
-	fmt.Println("Welcome to time manager")
-
-	// Get the current time
+	fmt.Println("Welcome to time study of golang")
 	presentTime := time.Now()
+	fmt.Println(presentTime)
+	fmt.Println(presentTime.Format("01-02-2006 Monday 00:57:03.384592"))
+	fmt.Println(presentTime.Format("01-02-2006 Monday")) //this is the way in go you have to specify formatting of date
 
-	// Format the time for better readability
-	formattedTime := presentTime.Format("2006-01-02 15:04:05") // YYYY-MM-DD HH:MM:SS
-	fmt.Println("Current time:", formattedTime)
+	createdDate := time.Date(2020, time.September, 10, 23, 43, 0, 0, time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday")) //you have to pass monday here to set the format
+
+	//go env tells you the formats it can create program in, an executable for any Os (_for MAC it is GOOS="darwin")
+	//use go build will compile after finding the main.go file even tough your are in any OS, it will give windows file
+	//%GOOS="windows" go build | GOOS="linux" go build
+	/*
+		Common Go Environment Variables:
+			GOROOT – Path to the Go installation.
+			GOPATH – Path for Go workspaces (where src, bin, and pkg reside).
+			GOBIN – Directory where compiled binaries are stored.
+			GOOS – Target operating system (e.g., linux, darwin, windows).
+			GOARCH – Target architecture (e.g., amd64, arm64).
+			GOMODCACHE – Directory for Go module cache.
+
+		Set an environment variable (temporarily for the session):
+			go env -w GO111MODULE=on
+	*/
 }
--- go.mod
+++
@@ -1,3 +0,0 @@
-module golang-fluff-stuff
-
-go 1.24.1
